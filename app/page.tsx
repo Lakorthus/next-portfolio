ed3858f5695d2ac8cab0cd6ff4dbee7af7d7fae5
Julio Velezmoro <julio28ve@gmail.com>
2023-02-17 22:00:38 +0000

    fix: responsive primary layout

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,6 @@ const Home = () => {
         <div className="gradient-03 z-0" />
         {/* <Explore /> */}
       </div>
-      <div className="relative">
-        {/* <GetStarted /> */}
-        <div className="gradient-04 z-0" />
-        {/* <WhatsNew /> */}
-      </div>
-      {/* <World /> */}
-      <div className="relative">
-        {/* <Insights /> */}
-        <div className="h-screen bg-red-300 z-0" />
-        {/* <Feedback /> */}
-      </div>
-      {/* <Footer /> */}
     </>
   );
 };
