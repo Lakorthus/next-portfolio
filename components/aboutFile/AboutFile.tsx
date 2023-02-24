4587454acd396a2e26368e2d1299512d718de425
Julio Velezmoro <julio28ve@gmail.com>
2023-02-24 18:05:36 +0000

    fix: list about file for lighthouse optimization

--- components/aboutFile/AboutFile.tsx
+++ components/aboutFile/AboutFile.tsx
@@ -29,46 +29,49 @@ const AboutFile: React.FC<IAboutFile> = ({
 }) => {
   return (
     <div className={`mt-1.5 flex flex-col lg:text-lg text-base`}>
-      <ul
+      <div
         onClick={() => setState(!state)}
-        className={`flex gap-1 cursor-pointer w-fit`}
+        className={`flex flex-col gap-1 cursor-pointer w-fit`}
       >
-        <span>{state ? <FiChevronDown /> : <FiChevronRight />}</span>
-        <span
-          style={{
-            color: `${
-              yellow
-                ? 'rgb(235,203,139)'
-                : blue
-                ? 'rgb(136,192,208)'
-                : red
-                ? 'rgb(191,97,106)'
-                : green
-                ? 'rgb(163,190,140)'
-                : 'whitesmoke'
-            }`,
-          }}
-        >
-          <BsFolderFill />
+        <span className="flex gap-1 cursor-pointer w-fit ">
+          {state ? <FiChevronDown /> : <FiChevronRight />}
+
+          <BsFolderFill
+            className="mx-1"
+            style={{
+              color: `${
+                yellow
+                  ? 'rgb(235,203,139)'
+                  : blue
+                  ? 'rgb(136,192,208)'
+                  : red
+                  ? 'rgb(191,97,106)'
+                  : green
+                  ? 'rgb(163,190,140)'
+                  : 'whitesmoke'
+              }`,
+            }}
+          />
+
+          <span> {title}</span>
         </span>
-        <span> {title}</span>
-      </ul>
-      {state && (
-        <AnimatePresence>
-          {listItems?.map((item, index) => (
-            <motion.li
-              className={`ml-7 list-none text-[#81A1C1]`}
-              initial="hidden"
-              animate="visible"
-              exit="go"
-              variants={variants}
-              key={index}
-            >
-              <BsMarkdownFill /> {item}
-            </motion.li>
-          ))}
-        </AnimatePresence>
-      )}
+        {state && (
+          <AnimatePresence>
+            {listItems?.map((item, index) => (
+              <motion.div
+                className={`flex items-center gap-2 ml-7 list-none text-[#81A1C1]`}
+                initial="hidden"
+                animate="visible"
+                exit="go"
+                variants={variants}
+                key={index}
+              >
+                <BsMarkdownFill /> {item}
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        )}
+      </div>
     </div>
   );
 };
