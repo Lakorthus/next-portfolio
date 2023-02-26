e60bea97c51f82789d191b16d0362889c27eea81
Julio Velezmoro <julio28ve@gmail.com>
2023-02-26 22:22:04 +0000

    fix: sanity path and previews paths

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -2,6 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function preview(req: NextApiRequest, res: NextApiResponse) {
   res.setPreviewData({});
-  res.writeHead(307, { Location: '/' });
+  res.writeHead(307, {
+    Location: '../../app/portfolio',
+  });
   res.end();
 }
