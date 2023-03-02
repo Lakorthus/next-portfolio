d6c074bcdc437bf6dfd73c0bb8116ca54afb4569
Julio Velezmoro <julio28ve@gmail.com>
2023-03-02 15:17:16 +0000

    build: jest config

---
+++ jest.setup.js
@@ -0,0 +1,6 @@
+//Optional configuration for jest before running tests
+//if you delete this file, you will need to remove 'setupFilesAterEnv' from jest.config.js
+
+//Used for __TEST__/testing library.js
+//learn more about testing library here: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
