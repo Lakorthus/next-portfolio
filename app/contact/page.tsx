d5f866af31a9f53543a6b6ff2ee46c2b865f5e3d
Julio Velezmoro <julio28ve@gmail.com>
2023-03-21 18:19:39 +0000

    fix: dark mode form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -25,7 +25,7 @@ const Contact = () => {
 
   return (
     <section className="yPaddings px-2 mx-auto max-w-screen-md">
-      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center">
         Contact Me
       </h2>
       <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
@@ -35,14 +35,14 @@ const Contact = () => {
         <div>
           <label
             htmlFor="name"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-300"
           >
             Your Name
           </label>
           <input
             type="name"
             id="name"
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
             placeholder="Julio Velezmoro"
             required
           />
@@ -50,14 +50,14 @@ const Contact = () => {
         <div>
           <label
             htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-300"
           >
             Your email
           </label>
           <input
             type="email"
             id="email"
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
             placeholder="julio28ve@gmail.com"
             required
           />
@@ -65,30 +65,30 @@ const Contact = () => {
         <div>
           <label
             htmlFor="subject"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-300"
           >
             Subject
           </label>
           <input
             type="text"
             id="subject"
-            className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+            className="block p-3 w-full text-sm text-gray-200 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
             name="subject"
-            placeholder="Let us know how we can help you"
+            placeholder="Let me know how I can help you"
             required
           />
         </div>
         <div className="sm:col-span-2">
           <label
             htmlFor="message"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-400"
           >
             Your message
           </label>
           <textarea
             id="message"
             rows={6}
-            className="block resize-none p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+            className="block resize-none p-2.5 w-full text-sm text-gray-200 bg-gray-50 rounded-lg shadow-sm border border-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
             name="message"
             placeholder="Leave a comment..."
             draggable="false"
--- sections/contact/Contact.tsx
+++ sections/contact/Contact.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import Link from 'next/link';
 
-import { fadeIn, staggerContainer, textVariant2 } from '../../utils/motion';
+import { fadeIn, staggerContainer } from '../../utils/motion';
 
 export interface IContact {}
 
@@ -12,15 +13,6 @@ const Contact: React.FC<IContact> = () => {
       id="contact"
       className="paddings relative z-10 flexCenter flex-col min-h-[95vh]"
     >
-      <motion.h2
-        variants={textVariant2}
-        initial="hidden"
-        whileInView="show"
-        className={`my-4 font-bold md:text-[40px] text-[34px] md:my-8 tracking-[15px]`}
-      >
-        Contact
-      </motion.h2>
-
       <motion.div
         variants={staggerContainer(0.1, 0.1)}
         initial="hidden"
@@ -37,17 +29,21 @@ const Contact: React.FC<IContact> = () => {
             alt="contact map"
             width={640}
             height={550}
-            className="w-full h-auto object-cover"
+            className="w-full h-auto object-cover absolute"
             priority
           />
-        </motion.div>
-        <div className="table w-full h-40  animate-pulse z-0">
-          <div className=" bg-neutral absolute top-0 left-0 right-0 bottom-0 m-auto w-full h-20 flex items-center justify-center overflow-hidden whitespace-nowrap monitor ">
-            <p className="monitorText">
-              JavaScript, React, Next.js, TypeScript, TailwindCSS, Framer Motion
-            </p>
+          <div className="table w-full h-40  animate-pulse z-0">
+            <div className="shadow-inset bg-neutral absolute top-0 left-0 right-0 bottom-0 m-auto w-full h-20 flex items-center justify-center overflow-hidden whitespace-nowrap monitor ">
+              <p className="monitorText">
+                Build with: JavaScript, React, Next.js, TypeScript, TailwindCSS,
+                Framer Motion
+              </p>
+            </div>
           </div>
-        </div>
+        </motion.div>
+        <Link href="./contact" className="btn btn-lg btn-primary z-20">
+          Contact me
+        </Link>
       </motion.div>
 
       <div className="gradient-03 z-10" />
